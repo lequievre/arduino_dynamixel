1296526ecce87e8a9cf6e2b882a4aa88fac447e8
lequievre <laurent.lequievre@gmail.com>
2019-02-20 18:48:44 +0100

    Adding joint state publisher

--- rqt_plugins/arduino/mobile/mobile.ino
+++ rqt_plugins/arduino/mobile/mobile.ino
@@ -1,5 +1,7 @@
 #include <DynamixelWorkbench.h>
 #include <ros.h>
+#include <sensor_msgs/JointState.h>
+#include <geometry_msgs/Twist.h>
 
 #if defined(__OPENCM904__)
   #define DEVICE_NAME "3" //Dynamixel on Serial3(USART3)  <-OpenCM 485EXP
@@ -12,8 +14,25 @@
 #define DXL_ID_WHEEL_RIGHT   3
 
 DynamixelWorkbench dxl_wb;
+ros::NodeHandle  nh;
+sensor_msgs::JointState joint_states_msg;
+ros::Publisher joint_states_pub("joint_states", &joint_states_msg, 100);
+
 
 std::map<std::string, uint8_t> map_id_wheel_dynamixels;
+float wheel_position[2];
+float wheel_velocity[2];
+float wheel_effort[2];
+
+char *wheel_names[] = {"wheel_right", "wheel_left" };
+
+void commandVelocityCallback(const geometry_msgs::Twist &msg)
+{
+  double robot_lin_vel = msg.linear.x;
+  double robot_ang_vel = msg.angular.z;
+}
+
+ros::Subscriber<geometry_msgs::Twist> cmd_vel_sub("cmd_vel", commandVelocityCallback );
 
 bool initWorkbench(const std::string port_name, const uint32_t baud_rate)
 {
@@ -35,14 +54,11 @@ bool initWorkbench(const std::string port_name, const uint32_t baud_rate)
   return result;
 }
 
-
-
 void getDynamixelsWheelInfo()
 {
    map_id_wheel_dynamixels["wheel_left"] = DXL_ID_WHEEL_LEFT;
    map_id_wheel_dynamixels["wheel_right"] = DXL_ID_WHEEL_RIGHT;
 }
-
 
 bool loadWheelDynamixels(void)
 {
@@ -227,6 +243,9 @@ void setup() {
   Serial.begin(57600);
   while(!Serial); // Wait for Opening Serial Monitor
 
+  nh.advertise(joint_states_pub);
+  nh.subscribe(cmd_vel_sub);
+
   if (!initWorkbench(DEVICE_NAME,BAUDRATE)) return;
   
   getDynamixelsWheelInfo();
@@ -249,5 +268,27 @@ void loop() {
     Serial.println(log);
     Serial.println("Failed to Read Bulk Data !");
   }
+
+  wheel_position[0] = present_data[0]; // wheel right position
+  wheel_position[1] = present_data[1]; // wheel left position
+
+  wheel_velocity[0] = present_data[2]; // wheel right velocity
+  wheel_velocity[1] = present_data[3]; // wheel left velocity
+
+  wheel_effort[0] = present_data[4]; // wheel right effort
+  wheel_effort[1] = present_data[5]; // wheel left effort
+  
+
+  joint_states_msg.header.stamp = ros::Time::now();
+  joint_states_msg.velocity_length = 2;
+  joint_states_msg.position_length = 2;
+  joint_states_msg.effort_length = 2;
+  joint_states_msg.name_length = 2;
+  joint_states_msg.position = wheel_position;
+  joint_states_msg.velocity = wheel_velocity;
+  joint_states_msg.effort = wheel_effort;
+  joint_states_msg.name = wheel_names;
+  
+  joint_states_pub.publish(&joint_states_msg);
 
 }
